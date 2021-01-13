fc3acb2404ca8f3642a9d5f2e19c6d3563a47c15
alexandreLamarre <alex7285@gmail.com>
2021-01-13 17:32:37 -0500

    disparity dot visualization

--- src/SortVisualizer.js
+++ src/SortVisualizer.js
@@ -22,6 +22,7 @@ class SortVisualizer extends React.Component{
       max_el: 2048,
       num_el: 512,
       data: [], //integer array to be sorted, etc...
+      sorted_data: [],
       running: false,
       algorithm: "insertion",
       realTime: 0,
@@ -31,6 +32,13 @@ class SortVisualizer extends React.Component{
       auxWrites: 0,
     };
     this.canvas = React.createRef();
+  }
+
+  draw(rand_arr, w, h, selected, sorted_arr){
+    if(this.state.type === "scatter") this.drawScatter(rand_arr, w, h, selected);
+    if(this.state.type === "swirl") this.drawSwirl(rand_arr, w, h, selected);
+    if(this.state.type === "disparity") this.drawDisparity(rand_arr, w, h,
+                                                            selected, sorted_arr);
   }
 
   /** When component Mounts set the canvas based on window dimensions.
@@ -46,10 +54,10 @@ class SortVisualizer extends React.Component{
     for(let i = 0; i < this.state.num_el; i++){
       rand_arr.push(Math.random());
     }
-    if(this.state.type === "scatter") this.drawScatter(rand_arr, w, h);
+    let sorted_arr = rand_arr.slice().sort();
 
-    else if (this.state.type === "swirl") this.drawSwirl(rand_arr, w, h);
-    this.setState({data:rand_arr, width: w, height: h})
+    this.draw(rand_arr, w, h, [], sorted_arr);
+    this.setState({data:rand_arr, width: w, height: h, sorted_data: sorted_arr});
   }
 
   drawScatter(rand_arr, w, h, selected){
@@ -89,8 +97,8 @@ class SortVisualizer extends React.Component{
     const ctx = this.canvas.current.getContext("2d");
     var highlight = false;
     ctx.clearRect(0, 0, w, h);
-    const max_r = Math.min(w/2, h/2);
-    const center = {x:w/2,y:h/2};
+    const max_r = Math.min(w/2, (h*0.95)/2);
+    const center = {x:w/2,y:h*0.95/2};
     for(let i = 0; i < rand_arr.length; i++){
       ctx.beginPath();
       if(selected !== undefined && selected !== null){
@@ -123,6 +131,55 @@ class SortVisualizer extends React.Component{
     }
   }
 
+  drawDisparity(rand_arr, w, h, selected, sorted_data){
+    const ctx = this.canvas.current.getContext("2d");
+    var highlight = false;
+    ctx.clearRect(0, 0, w, h);
+    const max_r = Math.min(w/2, h*0.95/2);
+    const c = rand_arr.length;
+    const center = {x:w/2,y:h*0.95/2};
+    const num_el = rand_arr.length;
+    for(let i = 0; i < rand_arr.length; i++){
+      const new_color = assignColorGradient(rand_arr[i]);
+      //closeness measures the distance from the center:
+      // a distance of 1 means the element is in the correct place in the
+      //sorted array
+      // a distance of 0 means it is the furthest possible from the correct place
+      //in the sorted array
+      const sorted_index = sorted_data.indexOf(rand_arr[i]);
+      const closeness = c - Math.abs(i-sorted_index);
+      ctx.beginPath();
+      if(selected !== undefined && selected !== null){
+        for(let j = 0; j < selected.length; j++){
+          if(selected[j] === i) highlight = true;
+        }
+      }
+      ctx.fillStyle = new_color;
+      const theta = (i/rand_arr.length)*Math.PI*2;
+      const r = ((max_r)*closeness/c);
+      const xPos = center.x + r*Math.cos(theta);
+      const yPos = center.y + r*Math.sin(theta);
+      ctx.arc(xPos,
+              yPos,
+              1,
+              0,
+              2*Math.PI);
+      ctx.fill();
+      ctx.closePath();
+      if(highlight === true){
+        const x = xPos-6;
+        const y = yPos-6;
+        ctx.beginPath();
+        ctx.strokeStyle = "rgb(255, 255, 255)";
+        ctx.rect(x,y,12,12);
+        ctx.stroke();
+        ctx.closePath();
+        highlight = false;
+      }
+    }
+
+  }
+
   /**
   **/
   resetData(){
@@ -132,12 +189,13 @@ class SortVisualizer extends React.Component{
 
     const rand_arr = [];
     for(let i = 0; i < this.state.num_el; i++){
-      rand_arr.push(Math.random());
+      rand_arr.push(Math.random()*0.95);
     }
-    if(this.state.type === "scatter") this.drawScatter(rand_arr, w, h);
-    else if(this.state.type === "swirl") this.drawSwirl(rand_arr, w, h);
-    this.setState({data:rand_arr});
-    //draw new random data in swirl dot plot
+    const selected = []; //no elements initially selected
+
+    let sorted_arr = rand_arr.slice().sort();
+    this.draw(rand_arr, w, h, selected, sorted_arr);
+    this.setState({data:rand_arr, sorted_data: sorted_arr});
 
   }
 
@@ -152,6 +210,10 @@ class SortVisualizer extends React.Component{
     else if(e.target.value === "swirl"){
       this.setState({type: e.target.value});
       this.drawSwirl(this.state.data, w, h);
+    }
+    else if(e.target.value === "disparity"){
+      this.setState({type: e.target.value});
+      this.drawDisparity(this.state.data, w ,h, [], this.state.sorted_data);
     }
   }
 
@@ -199,8 +261,7 @@ class SortVisualizer extends React.Component{
                   animations[cur_index].select !== undefined){
                     selected = animations[cur_index].select;
                   }
-      if(this.state.type === "scatter") this.drawScatter(rand_arr, w, h, selected);
-      else if(this.state.type === "swirl") this.drawSwirl(rand_arr, w, h, selected);
+      this.draw(rand_arr, w, h, selected, this.state.sorted_data);
       // continue animation when possible
       if(cur_index < animations.length) requestAnimationFrame( () => {
                                             this.animate(rand_arr, w, h, animations, cur_index+1)})
@@ -254,6 +315,7 @@ class SortVisualizer extends React.Component{
                     value = {this.state.type}>
               <option value="scatter"> Scatter Plot</option>
               <option value="swirl"> Swirl Dots</option>
+              <option value = "disparity"> Disparity Dots </option>
             </select>
           </p>
           <p>
@@ -274,6 +336,7 @@ class SortVisualizer extends React.Component{
               </optgroup>
               <optgroup label = "Exchange Family">
                 <option value = "quick"> Quick Sort </option>
+                <option disabled = {true}> Stable Quick Sort </option>
                 <option value = "dualquick"> Dual Pivot Quick Sort</option>
               </optgroup>
               <optgroup label = "Non-Comparison Family">
@@ -303,16 +366,21 @@ class SortVisualizer extends React.Component{
         </Draggable>
         <Draggable>
           <div className = "animationControls" hidden = {this.state.running === false}>
+            <button> -inf </button>
+            <button> -1 </button>
             <button> Start/Pause </button>
             <button
             onClick = {() => this.clearAnimations()}>
               Stop
             </button>
+            <button> +1 </button>
+            <button> +inf </button>
             <p> Visualization type:
               <select onChange= {(e) => this.changePlotType(e)}
                       value = {this.state.type}>
-                <option value="scatter"> Scatter Plot</option>
-                <option value="swirl"> Swirl Dots</option>
+                <option value= "scatter"> Scatter Plot</option>
+                <option value= "swirl"> Swirl Dots</option>
+                <option value ="disparity"> Disparity Dots </option>
               </select>
             </p>
             <div> Real sorting time (with animations) <b> {this.state.realTime} ms</b> </div>
@@ -351,4 +419,64 @@ async function waitStartAnimate(that, rand_arr, w, h, animations, time){
     comparions: 0, realTime:time,
     swaps: 0});
   that.animate(rand_arr, w, h, animations, 0);
+}
+
+function quickSorted(arr, l, r){
+  if(l < r){
+    var pivot = partitioned(arr, l, r);
+
+    quickSorted(arr, l, pivot -1);
+    quickSorted(arr, pivot+1, r);
+  }
+}
+
+/**
+
+**/
+function partitioned(arr, l, r){
+  var pivot = arr[r];
+
+  let i = l - 1;
+  for(let j = l; j <= r-1; j++){
+    if(arr[j] < pivot){
+      i++;
+      //swap array[i] and array[j]
+      const temp = arr[j];
+      const temp2 = arr[i];
+      arr[j] = temp2;
+      arr[i] = temp;
+    }
+  }
+  //swap array[i+1] and arr[r]
+  const temp = arr[i+1];
+  const temp2 = arr[r];
+  arr[i+1] = temp2;
+  arr[r] = temp;
+  return (i + 1)
+}
+
+/**
+Assigns a input value in the continuous range 0 to 1, to
+a continuous color gradient
+@param value the data value to be converted to color
+**/
+function assignColorGradient(value){
+  if(value <0.33){
+    const r = 255 - (value)*3*255;
+    const g = value*3*255;
+    const b = 0;
+    return "rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+  }
+  else if(value < 0.67){
+    const r = 0;
+    const g = 255 - (value -0.33)*3*255;
+    const b = (value -0.33)*3*255;
+    return "rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+  }
+  else{
+    const r = (value-0.67)*3*255;
+    const g = 0;
+    const b = 255 - (value - 0.67)*3*255;
+    return "rgb(" + r.toString() + "," + g.toString() + "," + b.toString() + ")";
+  }
 }
