c9b935f419367e8ef858a0e5c59349b917199927
alexandreLamarre <alex7285@gmail.com>
2021-01-18 14:30:56 -0500

    fixed counting sort 0-bug

--- src/SortAlgorithms/Javascript/CountingSort.js
+++ src/SortAlgorithms/Javascript/CountingSort.js
@@ -5,7 +5,9 @@
 export default function getCountingSort(rand_arr){
   const arr = rand_arr.slice();
   const animations = [];
-  countingSort(arr, animations);
+  console.log("called on", rand_arr);
+  const rarr = countingSort(arr, animations);
+  console.log(rarr);
   return animations;
 }
 
@@ -17,26 +19,31 @@ function countingSort(arr , animations){
   const k = Math.max(...arr);
   const counts = [];
   const outputs = new Array(arr.length);
-  for(let i = 0; i < k; i++){
+  for(let i = 0; i <= arr.length; i++){
     counts.push(0);
   }
 
   for(let i = 0; i < arr.length; i++){
     animations.push({select: [i]});
     counts[arr[i]] ++;
+    if(arr[i] === undefined)console.log(arr[i]);
   }
+  // console.log("buckets pre", counts);
 
-  for(let i = 1; i < k; i++){
+  for(let i = 1; i <= k; i++){
     counts[i] = counts[i] + counts[i-1];
   }
-
-  for(let i = arr.length - 1; i >= 0; i--){
-    outputs[counts[arr[i]] -1 ] = arr[i];
+  // console.log("buckets", counts)
+  for(let i = 0; i < arr.length; i++){
+    if(0 === counts[arr[i]]) console.log(counts[arr[i]], i)
+    outputs[counts[arr[i]]] = arr[i];
     counts[arr[i]] --;
   }
-
   for(let i = 0; i < arr.length; i++){
+    if(outputs[i] === undefined) outputs[i] = 0;
     animations.push({set: [i, outputs[i]], select:[i]});
     arr[i] = outputs[i];
   }
+
+  return arr;
 }
